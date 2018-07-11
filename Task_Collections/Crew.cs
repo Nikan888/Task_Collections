f869e0e9a7fec83356f07ec7cbd71bfb273a9e51
Nikan <32056409+Nikan888@users.noreply.github.com>
2018-07-11 17:15:36 +0300

    task

--- Task_Collections/Crew.cs
+++ Task_Collections/Crew.cs
@@ -8,14 +8,14 @@ namespace Task_Collections
 {
     public class Crew : List<Worker>
     {
-        public override string ToString()
-        {
-            StringBuilder sb = new StringBuilder();
-            foreach (Worker w in this)
-            {
-                sb.AppendFormat("{0};{1};{2}", w.FirstName, w.Age, w.WorkPosition);
-            }
-            return sb.ToString();
-        }
+        //public override string ToString()
+        //{
+        //    StringBuilder sb = new StringBuilder();
+        //    foreach (Worker w in this)
+        //    {
+        //        sb.AppendFormat("{0};{1};{2}", w.FirstName, w.Age, w.WorkPosition);
+        //    }
+        //    return sb.ToString();
+        //}
     }
 }
